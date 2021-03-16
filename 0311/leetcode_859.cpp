8a8c364b00f52c1a788d57c8feb233ce7d43c910
xsun <sunxin89@gmail.com>
2021-03-16 14:24:16 -0700

    add

--- 0311/leetcode_859.cpp
+++ 0311/leetcode_859.cpp
@@ -1,4 +1,61 @@
 //
 // Created by Xin Sun on 2021-03-15.
+//Given two strings a and b, return true if you can swap two letters in a so the result is equal to b, otherwise, return false.
 //
+//Swapping letters is defined as taking two indices i and j (0-indexed) such that i != j and swapping the characters at a[i] and b[j]. For example, swapping at indices 0 and 2 in "abcd" results in "cbad".
+#include <iostream>
+using namespace std;
 
+class Solution{
+public:
+    bool hasDuplicates(string a){
+        int cnt[24] = {0};
+        for(int i = 0; a[i]; i++){
+            cnt[a[i] - 'a']++;
+            if(cnt[a[i] - 'a'] > 1){
+                return true;
+            }
+        }
+
+        return false;
+    }
+    bool buddyStrings(string a, string b) {
+        if(a == b) {
+            return hasDuplicates(a);
+        }
+        if(a.size() != b.size()) return false;
+        int p1 = 0, p2 = 0, q1 = 0, q2 = 0, cnt = 0;
+        for(int i = 0; a[i]; i++){
+            if(a[i] == b[i]) {
+                continue;
+            }
+            if(a[i] != b[i]){
+                cnt++;
+                if(cnt == 1){
+                    p1 = i;
+                    q1 = i;
+                }
+                if(cnt == 2){
+                    p2 = i;
+                    q2 = i;
+                }
+                if(cnt > 2){
+                    return false;
+                }
+            }
+
+        }
+        if(a[p1] == b[q2] && a[p2] == b[q1]){
+            return true;
+        }
+        return false;
+    }
+};
+int main()
+{
+    //"aaaaaaabc"
+    //"aaaaaaacb"
+    Solution s;
+    cout << s.buddyStrings("aaaaaaabc", "aaaaaaacb") << endl;
+    return 0;
+}
--- 0311/leetcode_860.cpp
+++ 0311/leetcode_860.cpp
@@ -1,4 +1,66 @@
 //
 // Created by Xin Sun on 2021-03-15.
+//At a lemonade stand, each lemonade costs $5.
 //
+//Customers are standing in a queue to buy from you, and order one at a time (in the order specified by bills).
+//
+//Each customer will only buy one lemonade and pay with either a $5, $10, or $20 bill.  You must provide the correct change to each customer, so that the net transaction is that the customer pays $5.
+//
+//Note that you don't have any change in hand at first.
+//
+//Return true if and only if you can provide every customer with correct change.
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int b5 = 0, b10 = 0, b20 = 0;
+        int i = 0;
+        while(i < bills.size()) {
+            if (bills[i] == 5) b5++;
+            if (bills[i] == 10) {
+                if (b5 == 0) {
+                    return false;
+                } else {
+                    b10++;
+                    b5--;
+                }
+            }
+            if (bills[i] == 20) {
+                if (b10 > 0 && b5 > 0) {
+                    b20++;
+                    b10--;
+                    b5--;
+                } else if (b10 > 0 && b5 == 0) {
+                    return false;
+                } else if (b10 == 0 && b5 > 2) {
+                    b5 -= 3;
+                    b20++;
+                } else {
+                    return false;
+                }
+            }
+            i++;
+        }
+        return true;
+    }
+};
+
+int main()
+{
+    Solution s;
+    vector<int> v;
+    //[5,5,5,10,20]
+    v.push_back(5);
+    v.push_back(5);
+    v.push_back(5);
+    v.push_back(10);
+    v.push_back(20);
+    bool ret = s.lemonadeChange(v);
+    cout << ret << endl;
+    return 0;
+}
 

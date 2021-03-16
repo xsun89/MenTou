0be289ddbd41e230995695acd99ea7eba022e7f3
xsun <sunxin89@gmail.com>
2021-03-16 16:31:36 -0700

    add

--- 0311/leetcode_338.cpp
+++ 0311/leetcode_338.cpp
@@ -1,4 +1,22 @@
 //
 // Created by Xin Sun on 2021-03-15.
 //
+#include <iostream>
+using namespace std;
 
+class Solution {
+public:
+    vector<int> countBits(int num) {
+        vector<int> ret(num + 1);
+        ret[0] = 0;
+        for (int i = 1; i <= num; i++){
+            ret[i] = ret[i & (i -1)] + 1;
+        }
+
+        return ret;
+    }
+};
+int main()
+{
+    return 0;
+}

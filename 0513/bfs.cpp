c7e335d8ca9133fe6bd25b93350eefa80dedbe64
xsun89 <sunxin89@gmail.com>
2021-05-26 10:27:17 -0700

    qq

---
+++ 0513/bfs.cpp
@@ -0,0 +1,24 @@
+//
+// Created by Xin Sun on 2021-05-26.
+//
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+struct node {
+  int x, y, step;
+};
+
+int n, m, sx, sy;
+int dir[4][2] = {0, 1, 1, 0, 0 -1, -1, 0};
+char mmap[105][105];
+
+int main() {
+    cin >> n >> m;
+    for(int i = 1; i <= n; i++){
+
+    }
+
+    return 0;
+}
